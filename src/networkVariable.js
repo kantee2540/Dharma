e91a96e6397c2f9dd578bf893d7e0d1d9108250a
Kantee Charoensedtasin <kantee2540@hotmail.com>
2021-02-23 00:05:48 +0700

    path to api

--- src/networkVariable.js
+++ src/networkVariable.js
@@ -1,5 +1,5 @@
 
-const baseUrl = "http://54.255.65.119";
+const baseUrl = "http://api.xn--12ca1b1ad6at4bbyx3hva0b9qja8g.com";
 const resourceUrl = baseUrl+"/sound_resource";
 
 export { baseUrl, resourceUrl };
