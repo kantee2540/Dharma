f03962dac1f39ffd43c533e8b093aeca1b3d579b
Kantee Charoensedtasin <kantee2540@hotmail.com>
2021-03-02 12:35:38 +0700

    route to production

--- src/networkVariable.js
+++ src/networkVariable.js
@@ -1,5 +1,5 @@
 
-const baseUrl = "http://127.0.0.1:8080";
-const resourceUrl = baseUrl+"/file";
+const baseUrl = "http://api.xn--12ca1b1ad6at4bbyx3hva0b9qja8g.com:8080";
+const resourceUrl = "http://api.xn--12ca1b1ad6at4bbyx3hva0b9qja8g.com/sound_resource";
 
 export { baseUrl, resourceUrl };
